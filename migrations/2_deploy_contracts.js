00f779a260930ded9dd19cf17a9dec64cd84d1a4
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2022-07-12 20:08:03 +0100

    Created modifiers for base contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
 const ProducerRole = artifacts.require("./ProducerRole.sol");
 const ConsumerRole = artifacts.require("./ConsumerRole.sol");
+const SupplyChain = artifacts.require("./SupplyChain");
 
 module.exports = function (deployer) {
   deployer.deploy(ProducerRole);
   deployer.deploy(ConsumerRole);
+  deployer.deploy(SupplyChain);
 };
