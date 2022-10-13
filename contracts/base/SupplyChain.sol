9522d6af991dc826f5e64c52cacab188987393df
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2022-10-13 21:05:32 +0100

    Moved contract location

--- contracts/base/SupplyChain.sol
+++ contracts/base/SupplyChain.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "../../contracts/core/Ownable.sol";
+import "../../contracts/access-control/Ownable.sol";
 import "../../contracts/access-control/ProducerRol.sol";
 import "../../contracts/access-control/QualityController.sol";
 
